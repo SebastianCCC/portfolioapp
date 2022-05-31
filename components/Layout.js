22b4def7122f9e940e5deffd90ca0f2be9b4b492
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-05-31 09:42:42 +0200

    add z index to header

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import SideNavBar from './Header/SideNavBar'
 const Layout = ({ children }) => {
   return (
     <>
-      <header className="relative">
+      <header className="relative z-[25]">
         <SideNavBar />
       </header>
       <main className="min-w-full min-h-screen">{children}</main>
