c069f49685534b1f988c4b739b8b5df42705ffb2
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-05-31 10:27:23 +0200

    add seo, favicon and routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,20 @@
 import Layout from '../components/Layout'
 import { StateProvider } from '../hooks/StateContext'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StateProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <StateProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </StateProvider>
+    </>
   )
 }
 
