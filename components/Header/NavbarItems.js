a3067820b878c153b5dc963e4743f9b3de9a8406
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-06-07 17:17:29 +0200

    fix header text missing on smaller screens

--- components/Header/NavbarItems.js
+++ components/Header/NavbarItems.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { motion } from 'framer-motion'
+import { StateContext } from '../../hooks/StateContext'
 
 const NavBarItems = ({ link, title, icon, socLink }) => {
+  const { isOpen, setIsOpen } = useContext(StateContext)
   const [isHover, setIsHover] = useState(false)
   return (
     <li
@@ -14,12 +16,12 @@ const NavBarItems = ({ link, title, icon, socLink }) => {
         <Link href={link}>
           <div className="flex items-center relative">
             <div className="hover:bg-purple rounded p-1">{icon}</div>
-            {isHover && (
+            {(isHover || isOpen) && (
               <motion.h2
                 initial={{ x: -20 }}
                 animate={{ x: 0 }}
                 transition={{ duration: 0.5 }}
-                className="pl-1 absolute left-[50px] w-screen"
+                className={`pl-1 ${!isOpen && 'absolute w-screen'} left-[50px]`}
               >
                 {title}
               </motion.h2>
