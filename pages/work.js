ff6dbe1563e3faba940a424d5706083ba131ebc5
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-06-11 19:45:18 +0200

    add work page, initial commit

---
+++ pages/work.js
@@ -0,0 +1,9 @@
+export default function Work() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-pink text-4xl">Hello Page World</h1>
+      </div>
+    </>
+  )
+}
