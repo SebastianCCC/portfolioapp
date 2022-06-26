d8b2de67952f6ca515b2e8025d27a9b1297233cd
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-06-26 14:08:42 +0200

    add infinity horizontal scroll on comStack

---
+++ components/ComStack/Tech.js
@@ -0,0 +1,14 @@
+const Tech = ({ stack }) => {
+  return (
+    <div className="flex justify-between w-fit sm:w-full lg:px-5">
+      {stack.map(({ name, icon }, i) => (
+        <article className="flex flex-col items-center justify-center p-2 select-none" key={i}>
+          <div className="text-3xl">{icon}</div>
+          <p>{name}</p>
+        </article>
+      ))}
+    </div>
+  )
+}
+
+export default Tech
