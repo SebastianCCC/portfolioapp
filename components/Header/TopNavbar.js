5b02bdca66915546cee6ca8709e4b4e8bc1af19f
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-07-17 20:58:25 +0200

    change topNavBar on mobile

--- components/Header/TopNavbar.js
+++ components/Header/TopNavbar.js
@@ -1,40 +1,19 @@
-import { useTheme } from 'next-themes'
 import Link from 'next/link'
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { StateContext } from '../../hooks/StateContext'
 import { LogoIconDark, MenuIcon } from './images'
 
 const SideNavBar = () => {
   const { isOpen, setIsOpen } = useContext(StateContext)
-  const [navbar, setNavbar] = useState(false)
-  const { theme } = useTheme()
-
-  const changeBackground = () => {
-    if (window.scrollY >= 72) {
-      setNavbar(true)
-    } else {
-      setNavbar(false)
-    }
-  }
-
-  useEffect(() => {
-    changeBackground()
-    window.addEventListener('scroll', changeBackground)
-  })
-
   return (
     <>
-      <div
-        className={`absolute flex justify-between p-4 w-full items-center cursor-pointer ${
-          navbar && 'dark:bg-primary bg-tertiary drop-shadow-md'
-        } transition duration-700 ease-out xl:hidden`}
-      >
+      <div className="flex justify-between p-4 w-full items-center cursor-pointer xl:hidden bg-additional">
         <div onClick={() => setIsOpen(true)} className="dark:text-white text-secondary pr-2">
           <MenuIcon />
         </div>
         <Link href="/">
           <div>
-            <LogoIconDark FillRef={theme} />
+            <LogoIconDark />
           </div>
         </Link>
       </div>
