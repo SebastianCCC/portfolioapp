dc269ab374f4d1238f814c10e8abb7878d284143
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2023-02-05 20:03:55 +0100

    add AnimatePresence

--- components/Header/Popout.js
+++ components/Header/Popout.js
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useTheme } from 'next-themes'
 import { useContext } from 'react'
 import { StateContext } from '../../hooks/StateContext'
@@ -30,29 +30,34 @@ const SideNavBar = () => {
 
   return (
     <>
-      <motion.div
-        initial="closed"
-        animate={isOpen ? 'open' : 'closed'}
-        variants={variants}
-        className="fixed w-full dark:bg-additional/90 bg-secondary/90 min-h-screen p-4 backdrop-blur-[8px]"
-      >
-        <div className={`flex justify-between w-full items-center`}>
-          <div onClick={() => setIsOpen(false)} className="dark:text-secondary text-additional">
-            <ExitIcon />
-          </div>
-          <div className="dark:text-secondary text-additional">
-            <LogoIcon />
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <ul onClick={() => setIsOpen(false)}>
-            {NavLinks.map(({ title, link }, i) => (
-              <NavBarItems key={i} title={title} link={link} />
-            ))}
-          </ul>
-          <ThemeSwitch />
-        </div>
-      </motion.div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial="closed"
+            animate="open"
+            exit="closed"
+            variants={variants}
+            className="fixed w-full dark:bg-additional/90 bg-secondary/90 min-h-screen p-4 backdrop-blur-[8px]"
+          >
+            <div className={`flex justify-between w-full items-center`}>
+              <div onClick={() => setIsOpen(false)} className="dark:text-secondary text-additional">
+                <ExitIcon />
+              </div>
+              <div className="dark:text-secondary text-additional">
+                <LogoIcon />
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <ul onClick={() => setIsOpen(false)}>
+                {NavLinks.map(({ title, link }, i) => (
+                  <NavBarItems key={i} title={title} link={link} />
+                ))}
+              </ul>
+              <ThemeSwitch />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   )
 }
