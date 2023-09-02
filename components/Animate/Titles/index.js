ab6e999a8648328bda52f418ac07323f470e3da9
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2023-09-02 20:39:55 +0200

    change animation for every header title

--- components/Animate/Titles/index.js
+++ components/Animate/Titles/index.js
@@ -5,21 +5,22 @@ const HeaderTitles = ({ title }) => {
     <motion.header
       initial={{ x: -50, opacity: 0 }}
       whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true }}
       transition={{
         type: 'spring',
         delay: 0.2,
         duration: 1,
       }}
-      className="lg:flex items-center text-base tracking-[2px] dark:text-tertiary"
+      className="flex items-center text-base tracking-[2px] dark:text-tertiary"
     >
       <h2 className="uppercase pr-2 flex-shrink-0">{title}</h2>
       <motion.div
         initial={{ width: 0 }}
-        animate={{ width: '100%' }}
+        whileInView={{ width: '100%' }}
         transition={{
           type: 'spring',
-          delay: 0.8,
-          duration: 5,
+          delay: 0.7,
+          duration: 2.5,
         }}
         className="dark:bg-tertiary bg-additional h-[1px]"
       />
