8ea5a972b0df27af9f1ab4e924b0a3775b92ae4b
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2023-12-02 22:54:29 +0100

    fix link

--- components/Header/NavbarItems.js
+++ components/Header/NavbarItems.js
@@ -22,28 +22,30 @@ const NavBarItems = ({
   onClick,
 }) => {
   return (
-    <LinkItem link={link}>
-      <li
-        className={`${style} group dark:hover:text-white dark:text-sec_addition text-additional cursor-pointer ${
-          !style && 'xl:py-0 py-[15px]'
-        } text-sm capitalize tracking-[1px] mx-4`}
-        onMouseEnter={onHoverEnter}
-        onMouseLeave={onHoverLeave}
-        onClick={onClick}
-      >
-        <div className="xl:flex items-center">
-          {icon && <div className={`${styleIcon} rounded p-1`}>{icon}</div>}
-          <motion.h2
-            initial={{ x: -20 }}
-            animate={{ x: 0 }}
-            transition={{ duration: 0.5 }}
-            className={`dark:group-hover:text-white text-center xl:ml-2 font-extrabold`}
-          >
-            {title}
-          </motion.h2>
-        </div>
-      </li>
-    </LinkItem>
+    <div className='mx-4'>
+      <LinkItem link={link}>
+        <li
+          className={`${style} group dark:hover:text-white dark:text-sec_addition text-additional cursor-pointer ${
+            !style && 'xl:py-0 py-[15px]'
+          } text-sm capitalize tracking-[1px]`}
+          onMouseEnter={onHoverEnter}
+          onMouseLeave={onHoverLeave}
+          onClick={onClick}
+        >
+          <div className="xl:flex items-center">
+            {icon && <div className={`${styleIcon} rounded p-1`}>{icon}</div>}
+            <motion.h2
+              initial={{ x: -20 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.5 }}
+              className={`dark:group-hover:text-white text-center xl:ml-2 font-extrabold`}
+            >
+              {title}
+            </motion.h2>
+          </div>
+        </li>
+      </LinkItem>
+    </div>
   )
 }
 
