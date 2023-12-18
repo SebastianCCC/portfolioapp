b08a619b8d573bd1328e13d5cf3649b0da5fe306
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2023-12-18 22:22:01 +0100

    align text left side on mobile

--- components/Work/PreviewCard.js
+++ components/Work/PreviewCard.js
@@ -91,13 +91,13 @@ const PreviewCard = ({
               className="absolute inset-0"
             >
               <div
-                className={`px-2 flex flex-col ${
+                className={`px-4 flex flex-col ${
                   collapsed ? 'py-6 xSmall:py-12' : 'py-12'
-                } justify-end items-center text-center w-full h-full text-white select-none`}
+                } justify-end xSmall:items-center xSmall:text-center w-full h-full text-white select-none`}
               >
                 <p>{format(new Date(endDate), DATE_FORMAT_DA)}</p>
-                <h3 className="text-md font-bold w-[320px] xSmall:w-full truncate">{name}</h3>
-                <p className="opacity-80 w-[320px] xSmall:w-full truncate">{role}</p>
+                <h3 className="text-md font-bold xSmall:w-full truncate">{name}</h3>
+                <p className="opacity-80 xSmall:w-full truncate">{role}</p>
               </div>
             </Link>
           </SkeletonLoader>
