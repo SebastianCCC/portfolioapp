fca8788d8db8526a92ba003fe5db356056a1fb15
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2024-02-07 21:08:38 +0100

    add typescript with Forum service

---
+++ utils/errorParser.ts
@@ -0,0 +1,44 @@
+import { FirebaseError } from '@firebase/util'
+import { AxiosResponse } from 'axios'
+
+export type ErrorParser = {
+	message: string
+	status?: number | string
+	error: boolean
+}
+
+type Error = AxiosResponse & FirebaseError
+
+export const errorParser = (error?: Error): ErrorParser => {
+	const errorMessage = message(error)
+
+	return {
+		message: errorMessage,
+		status: error?.status || error?.code,
+		error: true,
+	}
+}
+
+const message = (error?: Error) => {
+	switch (error?.status || error?.code) {
+		case 401:
+		case 'auth/wrong-password':
+			return 'Incorrect email or password'
+
+		case 403:
+		case 'INVALID_ID_TOKEN':
+			return 'You are not authorized to access this content'
+
+		case 404:
+			return 'Looking so hard but could not find what you are looking for'
+
+		case 422:
+			return 'Please fill in the required fields correctly'
+
+		case 500:
+			return 'Something went wrong on the server'
+
+		default:
+			return 'Something went wrong'
+	}
+}
