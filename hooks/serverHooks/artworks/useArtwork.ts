4d7f8bd79ec3b1d948cc705fc00029f51a4f7835
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2024-02-08 10:16:47 +0100

    format code withh new prettier config

--- hooks/serverHooks/artworks/useArtwork.ts
+++ hooks/serverHooks/artworks/useArtwork.ts
@@ -4,23 +4,23 @@ import { getShots } from '../../../services/dribbbleService'
 type Schemas = Dribbble['schemas']
 
 export const callDribbbleShots = async () => {
-	let loading = false
-	let error: boolean | string = false
-	let shots: Schemas['Shot'][] = []
+  let loading = false
+  let error: boolean | string = false
+  let shots: Schemas['Shot'][] = []
 
-	const dribbbleShots = async () => {
-		loading = true
-		const dribbbleShots = await getShots()
+  const dribbbleShots = async () => {
+    loading = true
+    const dribbbleShots = await getShots()
 
-		if (!dribbbleShots.error) {
-			shots = dribbbleShots
-			loading = false
-		} else {
-			error = dribbbleShots.message
-			loading = false
-		}
-	}
+    if (!dribbbleShots.error) {
+      shots = dribbbleShots
+      loading = false
+    } else {
+      error = dribbbleShots.message
+      loading = false
+    }
+  }
 
-	await dribbbleShots()
-	return { error, loading, shots }
+  await dribbbleShots()
+  return { error, loading, shots }
 }
--- services/dribbbleService.ts
+++ services/dribbbleService.ts
@@ -7,8 +7,8 @@ type Schemas = Dribbble['schemas']
 type Shot = Schemas['Shot'][] & ErrorParserSchema
 
 export function getShots(): Promise<Shot> {
-	return authClient(DRIBBBLE_URL_API, process.env.DRIBBBLE_ACCESS_TOKEN)
-		.get(`/v2/user/shots`)
-		.then((res) => res.data)
-		.catch((err) => errorParser(err.response))
+  return authClient(DRIBBBLE_URL_API, process.env.DRIBBBLE_ACCESS_TOKEN)
+    .get(`/v2/user/shots`)
+    .then((res) => res.data)
+    .catch((err) => errorParser(err.response))
 }
