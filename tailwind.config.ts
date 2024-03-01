26d68b4e63c994940645809afc215ea12599d8c6
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2024-03-02 00:13:26 +0100

    update content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   darkMode: 'class',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
